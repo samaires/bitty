e5041c31b7d205dca8c1953b532c2156c32bd133
samaires <samaire@samaire.net>
2014-01-13 01:03:34 +0100

    Just testing shit. lol

--- controller.py
+++ controller.py
@@ -3,9 +3,7 @@ import time
 import sys
 import subprocess
 
-def bitty(torrent):
-	ses = lt.session()
-	ses.listen_on(6881,6891)
+def bitty(torrent, ses):
 
 	e = lt.bdecode(open(torrent, 'rb').read())
 	info = lt.torrent_info(e)
@@ -21,8 +19,14 @@ def bitty(torrent):
 					'downloading', 'finished', 'seeding', 'allocating']
 		
 		subprocess.call('clear', shell=True)
+		
 		print '%.2f%% complete (down: %.1f kb/s up: %1f kb/s peers: %d) %s' % \
 			(s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
 				s.num_peers, state_str[s.state])
+		
+		ses.save_state()
 
 		time.sleep(1)
+
+def get_status(ses):
+	ses.get_status()
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import controller as con
+import libtorrent as lt
+import time
+import sys
+import subprocess
 
-target_torrent = raw_input("Enter name of torrent file: ")
 
+ses = lt.session()
+ses.listen_on(6881,6891)
 
-con.bitty(target_torrent)
+while True:
+	input = raw_input("What now?")
+
+	if (input == "Download"):
+		target_torrent = raw_input("Enter name of torrent file: ")
+		con.bitty(target_torrent, ses)
+
+	if (input == "status"):
+		con.get_status(ses)
